2ff5d582c9db217d9ea94f7c4aecaa7b247dab17
subhanmalik <sbn10@S_MALIK_LAPTOP>
2026-04-16 20:44:10 +0200

    chore: folders restructuring

--- backend/src/main/java/com/findhabitat/BackendApplication.java
+++ backend/src/main/java/com/findhabitat/BackendApplication.java
@@ -1,4 +1,4 @@
-package com.findhabitat.backend;
+package com.findhabitat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
