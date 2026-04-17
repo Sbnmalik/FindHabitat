26daf9489f629429d32369b1db8c4bd7b0ba19de
subhanmalik <sbn10@S_MALIK_LAPTOP>
2026-04-17 09:43:26 +0200

    chore: added empty lines and code formatting.

--- backend/src/main/java/com/findhabitat/dtos/HouseRequest.java
+++ backend/src/main/java/com/findhabitat/dtos/HouseRequest.java
@@ -9,34 +9,48 @@ public class HouseRequest {
     @NotBlank
     @Size(max = 200)
     private String addressLine;
+
     @NotBlank
     @Size(max = 100)
     private String city;
+
     @Size(max = 20)         
     private String postalCode;
+
     @NotBlank
     @Size(max = 50)
     private String propertyType;
+
     @NotBlank
     @Size(max = 50)
     private String ownershipStatus;
+
+    @Min(0)
     private Integer floorLevel;
+
     @NotNull
     private Boolean parkingAvailability;
+
     @Size(max = 2000)
     private String description;
+
     @NotNull
     @Min(0)
     private Integer bedrooms;
+
     @NotNull
     @Min(0)
     private Integer bathrooms;
+
     private LocalDate moveInDate;
+
     @NotNull
-    @DecimalMin("0.00")
+    @DecimalMin(value = "0.0", inclusive = true)
     private BigDecimal monthlyPrice;
-    @DecimalMin("0.00")
+    
+    @DecimalMin(value = "0.0", inclusive = false)
     private BigDecimal livingAreaSqm;
+
     private Boolean isAvailable;
 
 }
