9b5c6f23faebd107cb31366613f58a6965843337
subhanmalik <sbn10@S_MALIK_LAPTOP>
2026-04-20 10:47:04 +0200

    feat: added header files and create function.

---
+++ frontend/src/views/House/CreateHouse.jsx
@@ -0,0 +1,7 @@
+import HouseForm from "../../components/HouseForm";
+import { useNavigate } from "react-router-dom";
+import {crateHouse} from "../api/houseApi";
+
+export default function CreateHouse() {
+    const navigate = useNavigate();
+}
