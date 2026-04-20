2293edc5870f9041b691f369e6cd8c4ecb55231f
subhanmalik <sbn10@S_MALIK_LAPTOP>
2026-04-20 10:31:19 +0200

    feat: initialize formData state in HouseForm component.

--- frontend/src/components/HouseForm.jsx
+++ frontend/src/components/HouseForm.jsx
@@ -1,5 +1,20 @@
 import {useState, useEffect} from 'react';
 
 export default function HouseForm({initialData, onSubmit, submitLabel}){
-    
+    const [formData, setFormData] = useState({
+    addressLine: "",
+    city: "",
+    postalCode: "",
+    propertyType: "",
+    ownershipStatus: "",
+    floorLevel: "",
+    parkingAvailability: false,
+    description: "",
+    bedrooms: 0,
+    bathrooms: 0,
+    moveInDate: "",
+    monthlyPrice: "",
+    livingAreaSqm: "",
+    isAvailable: true,
+    });
 }
