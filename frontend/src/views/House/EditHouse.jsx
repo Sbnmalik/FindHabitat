db39db773d883776fe3bd6e6715920f913e578eb
subhanmalik <sbn10@S_MALIK_LAPTOP>
2026-04-20 11:02:30 +0200

    feat: implement house loading functionality in EditHouse component.

--- frontend/src/views/House/EditHouse.jsx
+++ frontend/src/views/House/EditHouse.jsx
@@ -7,4 +7,18 @@ export default function EditHouse() {
     const { id } = useParams();
     const navigate = useNavigate();
     const [house, setHouse] = useState(null);
+
+      useEffect(() => {
+    async function loadHouse() {
+      try {
+        const data = await getHouseById(id);
+        setHouse(data);
+      } catch (error) {
+        console.error(error);
+        alert("Failed to load house");
+      }
+    }
+
+    loadHouse();
+  }, [id]);
 }
