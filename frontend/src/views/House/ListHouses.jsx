e7b071aa08e0c2dad9fc9b8790c86782d5d671bd
subhanmalik <sbn10@S_MALIK_LAPTOP>
2026-04-20 10:50:58 +0200

    feat: import files and default function.

---
+++ frontend/src/views/House/ListHouses.jsx
@@ -0,0 +1,7 @@
+import { useEffect,useState } from "react";
+import { deleteHouse, getAllHouses } from "../api/houseApi";
+import { Link } from "react-router-dom";
+
+export default function ListHouses() {
+    
+}
