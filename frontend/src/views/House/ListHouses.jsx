fb26c1ff1a20a2b44233bf43d3b07dd4c579363e
subhanmalik <sbn10@S_MALIK_LAPTOP>
2026-04-20 10:51:54 +0200

    feat: implement house fetching logic in ListHouses component.

--- frontend/src/views/House/ListHouses.jsx
+++ frontend/src/views/House/ListHouses.jsx
@@ -3,5 +3,18 @@ import { deleteHouse, getAllHouses } from "../api/houseApi";
 import { Link } from "react-router-dom";
 
 export default function ListHouses() {
-    
+    const [houses, setHouses] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    async function fetchHouses() {
+        try {
+            const data = await getAllHouses();
+            setHouses(data);
+        } catch (error) {
+            console.error(error);
+            alert("Failed to fetch houses");
+        } finally {
+            setLoading(false);
+        }   
+    }
 }
