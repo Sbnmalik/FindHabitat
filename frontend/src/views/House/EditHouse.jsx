d1706487f92d43c7a19746d0506366efada06ef8
subhanmalik <sbn10@S_MALIK_LAPTOP>
2026-04-20 11:01:24 +0200

    feat: added header files and edit function.

---
+++ frontend/src/views/House/EditHouse.jsx
@@ -0,0 +1,8 @@
+import HouseForm from "../../components/HouseForm";
+import { useNavigate, useParams } from "react-router-dom";
+import { getHouseById, updateHouse } from "../api/houseApi";
+import { useEffect,useState } from "react";
+
+export default function EditHouse() {
+    
+}
