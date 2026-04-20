1dffc2ff61bf009d4d115cf092d2741b46029d16
subhanmalik <sbn10@S_MALIK_LAPTOP>
2026-04-20 11:08:52 +0200

    feat: implement house API functions skeletons.

--- frontend/src/api/houseApi.js
+++ frontend/src/api/houseApi.js
@@ -1,2 +1,17 @@
 const BASE_URL = 'http://localhost:8081/api/houses';
 
+export async function getAllHouses() {
+
+}
+export async function getHouseById() {
+
+}
+export async function createHouse() {
+
+}
+export async function updateHouse() {
+
+}
+export async function deleteHouse() {
+
+}
