6861db44b6b24cae3e7b46e5f8d68d131537ed46
subhanmalik <sbn10@S_MALIK_LAPTOP>
2026-04-20 11:11:20 +0200

    feat: function parameterized.

--- frontend/src/api/houseApi.js
+++ frontend/src/api/houseApi.js
@@ -3,15 +3,15 @@ const BASE_URL = 'http://localhost:8081/api/houses';
 export async function getAllHouses() {
 
 }
-export async function getHouseById() {
+export async function getHouseById(id) {
 
 }
-export async function createHouse() {
+export async function createHouse(houseData) {
 
 }
-export async function updateHouse() {
+export async function updateHouse(id, houseData) {
 
 }
-export async function deleteHouse() {
+export async function deleteHouse(id) {
 
 }
