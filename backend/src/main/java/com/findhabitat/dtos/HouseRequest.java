1254412381076f16e6b7883fe8a5cce4a85f95ff
subhanmalik <sbn10@S_MALIK_LAPTOP>
2026-04-30 16:17:46 +0200

    feat: add lombok annotations and remove get/set methods.

--- backend/src/main/java/com/findhabitat/dtos/HouseRequest.java
+++ backend/src/main/java/com/findhabitat/dtos/HouseRequest.java
@@ -1,12 +1,22 @@
 package com.findhabitat.dtos;
 
 import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 // import lombok.Getter;
 // import lombok.Setter;
 // use @Getter and @Setter annotations from Lombok to generate getters and setters automatically
-
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class HouseRequest {
     
     @NotBlank
@@ -56,88 +66,4 @@ public class HouseRequest {
 
     private Boolean isAvailable;
 
-    public String getAddressLine() {
-        return addressLine;
-    }
-    public void setAddressLine(String addressLine) {
-        this.addressLine = addressLine;
-    }
-    public String getCity() {
-        return city;
-    }
-    public void setCity(String city) {
-        this.city = city;
-    }
-    public String getPostalCode() {
-        return postalCode;
-    }
-    public void setPostalCode(String postalCode) {
-        this.postalCode = postalCode;
-    }
-    public String getPropertyType() {
-        return propertyType;
-    }
-    public void setPropertyType(String propertyType) {
-        this.propertyType = propertyType;
-    }
-    public String getOwnershipStatus() {
-        return ownershipStatus;
-    }
-    public void setOwnershipStatus(String ownershipStatus) {
-        this.ownershipStatus = ownershipStatus;
-    }
-    public Integer getFloorLevel() {
-        return floorLevel;
-    }
-    public void setFloorLevel(Integer floorLevel) {
-        this.floorLevel = floorLevel;
-    }
-    public Boolean getParkingAvailability() {
-        return parkingAvailability;
-    }
-    public void setParkingAvailability(Boolean parkingAvailability) {
-        this.parkingAvailability = parkingAvailability;
-    }
-    public String getDescription() {
-        return description;
-    }
-    public void setDescription(String description) {
-        this.description = description;
-    }
-    public Integer getBedrooms() {
-        return bedrooms;
-    }
-    public void setBedrooms(Integer bedrooms) {
-        this.bedrooms = bedrooms;
-    }
-    public Integer getBathrooms() {
-        return bathrooms;
-    }
-    public void setBathrooms(Integer bathrooms) {
-        this.bathrooms = bathrooms;
-    }
-    public LocalDate getMoveInDate() {
-        return moveInDate;
-    }
-    public void setMoveInDate(LocalDate moveInDate) {
-        this.moveInDate = moveInDate;
-    }
-    public BigDecimal getMonthlyPrice() {
-        return monthlyPrice;
-    }   
-    public void setMonthlyPrice(BigDecimal monthlyPrice) {
-        this.monthlyPrice = monthlyPrice;
-    }
-    public BigDecimal getLivingAreaSqm() {
-        return livingAreaSqm;
-    }
-    public void setLivingAreaSqm(BigDecimal livingAreaSqm) {
-        this.livingAreaSqm = livingAreaSqm;
-    }
-    public Boolean getIsAvailable() {
-        return isAvailable;
-    }
-    public void setIsAvailable(Boolean isAvailable) {
-        this.isAvailable = isAvailable;
-    }
 }
