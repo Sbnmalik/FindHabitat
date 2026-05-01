94a679b6eb81d53f2947e87b1f097d1b99ea7624
subhanmalik <sbn10@S_MALIK_LAPTOP>
2026-05-01 15:39:06 +0200

    style: remove unused header files and remove empty lines.

--- backend/src/main/java/com/findhabitat/controllers/HouseController.java
+++ backend/src/main/java/com/findhabitat/controllers/HouseController.java
@@ -1,10 +1,6 @@
 package com.findhabitat.controllers;
 
-import com.findhabitat.services.HouseService;
-import jakarta.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.findhabitat.dtos.HouseRequest;
