8a548c5f255a082283b3dbbcff939b6a7eac058b
subhanmalik <sbn10@S_MALIK_LAPTOP>
2026-05-05 14:53:26 +0200

    refactor: update import statement for createHouse function in CreateHouse component.

--- frontend/src/views/House/CreateHouse.jsx
+++ frontend/src/views/House/CreateHouse.jsx
@@ -3,7 +3,7 @@ import Button from "../../components/ui/Button";
 import { notifyError, notifySuccess } from "../../utils/toast";
 import "../../styles/HouseForm.css";
 import { useNavigate } from "react-router-dom";
-import createHouse from "../../api/houseApi";
+import {createHouse} from "../../api/houseApi";
 
 export default function CreateHouse() {
     const navigate = useNavigate();
