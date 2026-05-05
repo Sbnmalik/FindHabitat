ceb24dc8ccecd85bdca9640be8af6ace1c7d8ab7
subhanmalik <sbn10@S_MALIK_LAPTOP>
2026-05-05 13:15:47 +0200

    feat: add import files and set variables.

--- frontend/src/views/House/CreateHouse.jsx
+++ frontend/src/views/House/CreateHouse.jsx
@@ -1,8 +1,24 @@
-import HouseForm from "../../components/layout/HouseForm";
 import { useNavigate } from "react-router-dom";
 import { createHouse } from "../../api/houseApi";
+import {useForm} from "react-hook-form";
+import Button from "../../components/ui/Button";
+import { notifyError, notifySuccess } from "../../utils/toast";
+import "./HouseForm.css";
+
 export default function CreateHouse() {
-    const navigate = useNavigate();
+    const {    
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    reset,
+  } = useForm({
+    defaultValues: {
+      bedrooms: 0,
+      bathrooms: 0,
+      parkingAvailable: "No",
+      available: true,
+    },
+  });
 
     async function handleCreate(data) {
         try {
