31a3c26f758303ee7984dc50ea731e164c171045
subhanmalik <sbn10@S_MALIK_LAPTOP>
2026-05-16 21:46:01 +0200

    feat: implement methods to find and check existence of AppUser by email

--- backend/src/main/java/com/findhabitat/repositories/AppUserRepository.java
+++ backend/src/main/java/com/findhabitat/repositories/AppUserRepository.java
@@ -3,6 +3,11 @@ package com.findhabitat.repositories;
 import com.findhabitat.entities.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public class AppUserRepository extends JpaRepository<AppUser, Long> {
-    
+        
+    Optional<AppUser> findByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
