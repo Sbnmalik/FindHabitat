5b7c05e4f20550da387c1e0494a662a0c7f89c2d
subhanmalik <sbn10@S_MALIK_LAPTOP>
2026-05-16 22:04:46 +0200

    feat: add AppUserRepository and PasswordEncoder dependencies to AuthService

--- backend/src/main/java/com/findhabitat/services/AuthService.java
+++ backend/src/main/java/com/findhabitat/services/AuthService.java
@@ -1,8 +1,13 @@
 package com.findhabitat.services;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import com.findhabitat.repositories.AppUserRepository;
 
 @Service
 public class AuthService {
     
+    private final AppUserRepository appUserRepository;
+    private final PasswordEncoder passwordEncoder;
 }
