bd47607835d6a45f2ba404af63708e36c68041e3
subhanmalik <sbn10@S_MALIK_LAPTOP>
2026-05-16 21:37:37 +0200

    feat: define AppUser entity with unique email constraint

--- backend/src/main/java/com/findhabitat/entities/AppUser.java
+++ backend/src/main/java/com/findhabitat/entities/AppUser.java
@@ -2,6 +2,13 @@ package com.findhabitat.entities;
 
 import jakarta.persistence.*;
 
+@Entity
+@Table(
+    name = "app_user",
+    uniqueConstraints = {
+        @UniqueConstraint(columnNames = "email")
+    }
+)
 public class AppUser {
     
 }
