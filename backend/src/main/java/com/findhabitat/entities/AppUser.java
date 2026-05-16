ff4c5d704d863c76379859b5cbedca5a67db41ff
subhanmalik <sbn10@S_MALIK_LAPTOP>
2026-05-16 21:42:20 +0200

    feat: add additional fields and annotations

--- backend/src/main/java/com/findhabitat/entities/AppUser.java
+++ backend/src/main/java/com/findhabitat/entities/AppUser.java
@@ -1,7 +1,15 @@
 package com.findhabitat.entities;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(
     name = "app_user",
@@ -10,5 +18,21 @@ import jakarta.persistence.*;
     }
 )
 public class AppUser {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String fullName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserRole role;
 }
