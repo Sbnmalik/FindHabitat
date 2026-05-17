be9e4b09c11b8e8b57fe3988e6cf6585c56adf1e
subhanmalik <sbn10@S_MALIK_LAPTOP>
2026-05-17 12:39:19 +0200

    feat: implement custom service and import header files.

--- backend/src/main/java/com/findhabitat/services/CustomUserDetailsService.java
+++ backend/src/main/java/com/findhabitat/services/CustomUserDetailsService.java
@@ -1,5 +1,29 @@
 package com.findhabitat.services;
 
-public class CustomUserDetailsService {
-    
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import com.findhabitat.entities.AppUser;
+import com.findhabitat.repositories.AppUserRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService{
+    private final AppUserRepository appUserRepository;
+
+    public CustomUserDetailsService(AppUserRepository appUserRepository) {
+        this.appUserRepository = appUserRepository;
+    }
+    @Override
+        public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        AppUser appUser = appUserRepository.findByEmail(email.toLowerCase())
+            .orElseThrow(() -> new UsernameNotFoundException("User not found."));
+
+        return User.builder()
+            .username(appUser.getEmail())
+            .password(appUser.getPassword())
+            .roles(appUser.getRole().name())
+            .build();
+    }
 }
