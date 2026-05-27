8895b6dd4cf7c2e9ef5cfe49cd1041b72c8004ae
subhanmalik <sbn10@S_MALIK_LAPTOP>
2026-05-27 09:18:19 +0200

    feat: add reusable encoder and decoder provided my spring security

--- backend/src/main/java/com/findhabitat/config/JwtConfig.java
+++ backend/src/main/java/com/findhabitat/config/JwtConfig.java
@@ -1,6 +1,19 @@
 package com.findhabitat.config;
 
+import java.nio.charset.StandardCharsets;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+
+import com.nimbusds.jose.jwk.source.ImmutableSecret;
 
 @Configuration
 public class JwtConfig {
@@ -9,5 +22,22 @@ public class JwtConfig {
     public JwtConfig(JwtProperties jwtProperties) {
         this.jwtProperties = jwtProperties;
     }
-    
+    @Bean
+    SecretKey jwtSecretKey() {
+        return new SecretKeySpec(
+                jwtProperties.secret().getBytes(StandardCharsets.UTF_8),
+                "HmacSHA256"
+        );
+    }
+    @Bean
+    JwtEncoder jwtEncoder(SecretKey secretKey) {
+        return new NimbusJwtEncoder(new ImmutableSecret<>(jwtSecretKey()));
+    }
+    @Bean
+    JwtDecoder jwtDecoder(SecretKey jwtSecretKey) {
+        return NimbusJwtDecoder
+                .withSecretKey(jwtSecretKey)
+                .macAlgorithm(MacAlgorithm.HS256)
+                .build();
+    }   
 }
