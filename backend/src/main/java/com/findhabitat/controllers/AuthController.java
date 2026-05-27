113350a73b183d1bb7802cc4ace5d86acd5bfe07
subhanmalik <sbn10@S_MALIK_LAPTOP>
2026-05-27 10:06:23 +0200

    feat: remove unused import files and and replace the current method

--- backend/src/main/java/com/findhabitat/controllers/AuthController.java
+++ backend/src/main/java/com/findhabitat/controllers/AuthController.java
@@ -1,42 +1,35 @@
 package com.findhabitat.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.security.core.Authentication;
 
+import com.findhabitat.dtos.LoginRequest;
 import com.findhabitat.dtos.RegisterRequest;
-import com.findhabitat.entities.AppUser;
-import com.findhabitat.repositories.AppUserRepository;
-import com.findhabitat.response.UserResponse;
+import com.findhabitat.response.AuthResponse;
 import com.findhabitat.services.AuthService;
+
+import jakarta.validation.Valid;
 
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
-        private final AuthService authService;
-    private final AppUserRepository appUserRepository;
+    private final AuthService authService;
 
-    public AuthController(AuthService authService, AppUserRepository appUserRepository) {
+
+    public AuthController(AuthService authService) {
         this.authService = authService;
-        this.appUserRepository = appUserRepository;
     }
 
     @PostMapping("/register")
-    public UserResponse register(@RequestBody RegisterRequest request) {
+    public AuthResponse register(@Valid @RequestBody RegisterRequest request) {
         return authService.register(request);
     }
 
-    @GetMapping("/me")
-    public UserResponse getCurrentUser(Authentication authentication) {
-        String email = authentication.getName();
-
-        AppUser user = appUserRepository.findByEmail(email)
-            .orElseThrow(() -> new RuntimeException("Authenticated user not found."));
-
-        return UserResponse.fromEntity(user);
+    @PostMapping("/login")
+    public AuthResponse login(@Valid @RequestBody LoginRequest request) {
+        return authService.login(request);
     }
 }
