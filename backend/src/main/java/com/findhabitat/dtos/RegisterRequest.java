e476355f376000a9a6945b90858782e0ec6e6210
subhanmalik <sbn10@S_MALIK_LAPTOP>
2026-05-27 09:42:41 +0200

    style: refactor register model and add validations

--- backend/src/main/java/com/findhabitat/dtos/RegisterRequest.java
+++ backend/src/main/java/com/findhabitat/dtos/RegisterRequest.java
@@ -1,6 +1,9 @@
 package com.findhabitat.dtos;
 
 import com.findhabitat.entities.UserRole;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,8 +13,12 @@ import lombok.Setter;
 @NoArgsConstructor
 public class RegisterRequest {
     
+    @NotBlank(message = "Full name is required")
     public String fullName;
+    @NotBlank(message = "Email is required")
     public String email;
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters long")
     public String password;
     public UserRole role;
 }
